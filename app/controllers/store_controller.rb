ce7afc26ece04741fdccdce9466f7f3af519592c
jbrownrs <36203116+jbrownrs@users.noreply.github.com>
2018-05-21 18:50:11 +0100

    Adds cart button

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,11 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
   end
+
+  def increment_count
+    @counter = session[:counter]
+    # if counter is false or nil set to zero
+    @counter ||= 0
+    @counter += 1
+  end
 end
